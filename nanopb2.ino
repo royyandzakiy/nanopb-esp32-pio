136710ab7e8845f341be07cae11fa6352026f3e8
royyandzakiy <royyan.dzakiy@efishery.com>
2021-07-09 17:33:55 +0700

    feat:success encode & decode string with a hacky way. next: need to find a way to inject a string dynamically with encode_string(...)

--- nanopb2.ino
+++ nanopb2.ino
@@ -5,17 +5,48 @@
 #include "pb_encode.h"
 #include "pb_decode.h"
 
+bool encode_string(pb_ostream_t *stream, const pb_field_t *field, void * const *arg)
+{
+    char *str = "Hello, World!";
+    // char *str = *arg;
+    if (!pb_encode_tag_for_field(stream, field))
+        return false;
+    
+    return pb_encode_string(stream, (uint8_t*)str, strlen(str));
+}
+
+bool decode_string(pb_istream_t *stream, const pb_field_t *field, void **arg)
+{
+    uint8_t buffer[1024] = {0};
+    
+    /* We could read block-by-block to avoid the large buffer... */
+    if (stream->bytes_left > sizeof(buffer) - 1)
+        return false;
+    
+    if (!pb_read(stream, buffer, stream->bytes_left))
+        return false;
+    
+    /* Print the string, in format comparable with protoc --decode.
+     * Format comes from the arg defined in main().
+     */
+    Serial.print("Message.test_string: ");
+    Serial.println((char*)buffer);
+    //printf((char*)*arg, buffer);
+    return true;
+}
+
 void setup() {
 
   Serial.begin(115200);
 
-  uint8_t buffer[128];
+  uint8_t buffer[1280];
   bool status;
   size_t message_length;
 
   /* ENCODING */
   {
     TestMessage message = TestMessage_init_zero;
+    message.test_string.funcs.encode = encode_string;
   
     pb_ostream_t ostream = pb_ostream_from_buffer(buffer, sizeof(buffer));
   
@@ -46,10 +77,13 @@ void setup() {
   /* DECODING */
   {
     TestMessage out_message = TestMessage_init_zero;
-  
+    
     /* Create a stream that reads from the buffer. */
     pb_istream_t stream = pb_istream_from_buffer(buffer, message_length);
     
+    out_message.test_string.funcs.decode = &decode_string;
+    //printf((char*)out_message.test_string.arg);
+  
     /* Now we are ready to decode the message. */
     status = pb_decode(&stream, TestMessage_fields, &out_message);
   
--- src/test.pb.h
+++ src/test.pb.h
@@ -14,6 +14,7 @@ typedef struct _TestMessage {
     int32_t test_number; 
     int32_t test_number2; 
     int32_t test_number3; 
+    pb_callback_t test_string; 
 } TestMessage;
 
 
@@ -22,20 +23,22 @@ extern "C" {
 #endif
 
 /* Initializer values for message structs */
-#define TestMessage_init_default                 {0, 0, 0}
-#define TestMessage_init_zero                    {0, 0, 0}
+#define TestMessage_init_default                 {0, 0, 0, {{NULL}, NULL}}
+#define TestMessage_init_zero                    {0, 0, 0, {{NULL}, NULL}}
 
 /* Field tags (for use in manual encoding/decoding) */
 #define TestMessage_test_number_tag              1
 #define TestMessage_test_number2_tag             2
 #define TestMessage_test_number3_tag             3
+#define TestMessage_test_string_tag              4
 
 /* Struct field encoding specification for nanopb */
 #define TestMessage_FIELDLIST(X, a) \
 X(a, STATIC,   REQUIRED, INT32,    test_number,       1) \
 X(a, STATIC,   REQUIRED, INT32,    test_number2,      2) \
-X(a, STATIC,   REQUIRED, INT32,    test_number3,      3)
-#define TestMessage_CALLBACK NULL
+X(a, STATIC,   REQUIRED, INT32,    test_number3,      3) \
+X(a, CALLBACK, REQUIRED, STRING,   test_string,       4)
+#define TestMessage_CALLBACK pb_default_field_callback
 #define TestMessage_DEFAULT NULL
 
 extern const pb_msgdesc_t TestMessage_msg;
@@ -44,7 +47,7 @@ extern const pb_msgdesc_t TestMessage_msg;
 #define TestMessage_fields &TestMessage_msg
 
 /* Maximum encoded size of messages (where known) */
-#define TestMessage_size                         33
+/* TestMessage_size depends on runtime parameters */
 
 #ifdef __cplusplus
 } /* extern "C" */
